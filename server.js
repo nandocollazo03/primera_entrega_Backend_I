fa26ed83cce77f6af02ddd919266a13c65d1b5a1
Fernando Collazo <72205268+nandocollazo03@users.noreply.github.com>
2024-11-19 21:45:52 -0300

    Add files via upload

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const productsRouter = require('./routes/products');
+const cartsRouter = require('./routes/carts');
+
+const app = express();
+const PORT = 8080;
+
+app.use(express.json());
+
+app.use('/api/products', productsRouter);
+app.use('/api/carts', cartsRouter);
+
+app.listen(PORT, () => {
+    console.log(`El servidor está corriendo en el puerto: ${PORT}`);
+});
